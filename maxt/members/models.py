3b601c4e8c333c859da8a71f61e75107622e5e56
Christopher Flynn <flynnguy@gmail.com>
2018-10-20 00:09:49 -0400

    We should probably use Active instead of another field for active members

--- maxt/members/models.py
+++ maxt/members/models.py
@@ -8,7 +8,6 @@ class Member(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     authorized_tools = models.ManyToManyField(Tool, blank=True)
     rfid_tag_id = models.CharField(max_length=128) # TODO: figure out tag length
-    current_member = models.BooleanField(default=True)
 
     def __str__(self):
         return self.user.username
